5c33c3d22fce24eb3fab3ae7da1442135e84e435
unknown <merkulovwork@mail.ru>
2022-01-15 20:58:19 +1000

    added styles, navbar, routing by pages and new component - studentAdder with form for adding new student

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './student-list/student-list.component';
+import { StudentAdderComponent } from './student-adder/student-adder.component';
+
+const appRouters:Routes=[
+  {path:'', component:StudentListComponent},
+  {path:'add', component:StudentAdderComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentListComponent
+    StudentListComponent,
+    StudentAdderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRouters)
   ],
   providers: [],
   bootstrap: [AppComponent]
